3365c96f37e8351a69667fb0c686e3b65fffe842
Dino Kovač <dino.kovac@infinum.hr>
2014-12-01 22:43:21 +0100

    Implemented BT pixel cost calculation

--- main.cc
+++ main.cc
@@ -1,19 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
 
-int main( int argc, char** argv ) {
-    if (argc != 3) {
-     std::cerr << "Usage: " << argv[0] << " <first image> <second image>" << std::endl;
-     return -1;
+unsigned int calculatePixelCostOneWayBT(unsigned int leftCol, unsigned int rightCol, unsigned int row, const cv::Mat &leftImage, const cv::Mat &rightImage) {
+
+    unsigned char leftValue, rightValue, beforeRightValue, afterRightValue, rightValueMinus, rightValuePlus, rightValueMin, rightValueMax;
+
+    leftValue = leftImage.at<uchar>(row, leftCol);
+    rightValue = rightImage.at<uchar>(row, rightCol);
+
+    if (rightCol > 0) {
+        beforeRightValue = rightImage.at<uchar>(row, rightCol - 1);
+    } else {
+        beforeRightValue = rightValue;
     }
 
-    cv::Mat first_image;
-    cv::Mat second_image;
-    first_image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    second_image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    if (rightCol + 1 < rightImage.cols) {
+        afterRightValue = rightImage.at<uchar>(row, rightCol + 1);
+    } else {
+        afterRightValue = rightValue;
+    }
 
-    if(!first_image.data || !second_image.data) {
+    rightValueMinus = std::round((rightValue + beforeRightValue) / 2.f);
+    rightValuePlus = std::round((rightValue + afterRightValue) / 2.f);
+
+    rightValueMin = std::min(rightValue, std::min(rightValueMinus, rightValuePlus));
+    rightValueMax = std::max(rightValue, std::max(rightValueMinus, rightValuePlus));
+
+    return std::max(0, std::max((leftValue - rightValueMax), (rightValueMin - leftValue)));
+}
+
+unsigned int calculatePixelCostBT(unsigned int leftCol, unsigned int rightCol, unsigned int row, const cv::Mat &leftImage, const cv::Mat &rightImage) {
+    return std::min(calculatePixelCostOneWayBT(leftCol, rightCol, row, leftImage, rightImage),
+                    calculatePixelCostOneWayBT(rightCol, leftCol, row, rightImage, leftImage));
+}
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <first image> <second image>" << std::endl;
+        return -1;
+    }
+
+    cv::Mat firstImage;
+    cv::Mat secondImage;
+    firstImage = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    secondImage = cv::imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+
+    if(!firstImage.data || !secondImage.data) {
         std::cerr <<  "Could not open or find one of the images!" << std::endl;
         return -1;
     }
